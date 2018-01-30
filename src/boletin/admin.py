6f17fe54836d77f649ce892c50722eaa5a39cafc
andresshare <andresshare@gmail.com>
2018-01-30 18:45:18 -0500

    modified models & admin

--- src/boletin/admin.py
+++ src/boletin/admin.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
+from .models import Registrado
 
 # Register your models here.
+class AdminRegistrado(admin.ModelAdmin):
+    list_display =["email","nombre","timestamp"]
+    #list_display_links=["nombre"]
+    list_filter=["timestamp"]
+    list_editable=["nombre"]
+    search_fields=["email","nombre"]
+    class Meta:
+        model =Registrado
+
+
+
+admin.site.register(Registrado,AdminRegistrado)
--- src/boletin/models.py
+++ src/boletin/models.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
+
+
 # Create your models here.
+
+class Registrado(models.Model):
+
+    ''' Register Model '''
+
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
