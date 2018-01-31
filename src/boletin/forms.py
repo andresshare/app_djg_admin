23d2bd28298f4fcdc6f0d3276b51d03677ba046c
andresshare <andresshare@gmail.com>
2018-01-30 21:19:21 -0500

    modified button && form

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -1,14 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django import forms
 from .models import Registrado
 
+
 class RegModelForm(forms.ModelForm):
+
     class Meta:
-        model =Registrado
-        fields =["nombre","email"]
 
+        model = Registrado
+        fields = ['nombre', 'email']
 
-
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base,proveeder =email.split("@")
+        dominio,extension =proveeder.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Usar email con extension .edu")
+        return email
+    def Clean_nombre(self):
+        nombre = self.cleaned_data.get("nombre")
+        #validaciones
+        return nombre
 
 class RegForm(forms.Form):
-    nombre= forms.CharField(max_length=100)
-    email= forms.EmailField()
+
+    nombre = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+
+
+			
--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,26 +1,56 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 from django.shortcuts import render
-from .forms import RegForm
+
+from .forms import RegForm, RegModelForm
 from .models import Registrado
+
 
 # Create your views here.
 
 def inicio(request):
-    form = RegForm(request.POST or None)
-    if form.is_valid():
-        form_data=form.cleaned_data
-        abc=form_data.get("email")
-        abc2=form_data.get("nombre")
-        obj=Registrado.objects.create(email=abc,nombre=abc2)
-    
-        '''other way'''
-        #obj=Registrado()
-        #obj.email =abc
-        #obj.save()
+    titulo = 'Hola'
+    if request.user.is_authenticated:
+        titulo = 'Bienvenido %s' %(request.user)
+
+    form = RegModelForm(request.POST or None)
 
     contexto = {
-        'el_form': form,
+        'titulo': titulo, 
+        'el_form': form
         }
 
+    if form.is_valid():
+        instance = form.save(commit=False)
+        nombre=form.cleaned_data.get("nombre")
+        email=form.cleaned_data.get("email")
+
+        if not instance.nombre:
+            instance.nombre ="Persona"
+            instance.save()
+            contexto = {
+                'titulo': "Gracias %s!" %(nombre)
+                }
+        if not nombre:
+            contexto={
+                "titulo": "Gracias %s!"%(email)
+            }
+        print (instance)
+        print (instance.timestamp)
+        # form_data = form.cleaned_data
+        # abc = form_data.get('email')
+        # abc2 = form_data.get('nombre')
+        # obj = Registrado.objects.create(email=abc, nombre=abc2)
+
+        # obj=Registrado()
+        # obj.email =abc
+        # obj.save()
+
+    
+
     return render(request, 'inicio.html', contexto)
+
+
+
+			
