6216b2f2fc9414ea98d0a127c657a159b0bc54f7
andresshare <andresshare@gmail.com>
2018-01-30 20:03:15 -0500

    modified admin && forms

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .forms import RegForm
+from .models import Registrado
 
 # Create your views here.
+
+def inicio(request):
+    form = RegForm(request.POST or None)
+    if form.is_valid():
+        form_data=form.cleaned_data
+        abc=form_data.get("email")
+        abc2=form_data.get("nombre")
+        obj=Registrado.objects.create(email=abc,nombre=abc2)
+    
+        '''other way'''
+        #obj=Registrado()
+        #obj.email =abc
+        #obj.save()
+
+    contexto = {
+        'el_form': form,
+        }
+
+    return render(request, 'inicio.html', contexto)
--- src/pd110/urls.py
+++ src/pd110/urls.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """pd110 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +15,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from boletin import views
+#from boletin.views inicio
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('inicio/',views.inicio, name='inicio')
+    ]
